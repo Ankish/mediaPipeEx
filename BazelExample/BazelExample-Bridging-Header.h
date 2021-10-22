985f0830cc9f157fe021352cf0d640f909e94611
Ankish Jain <ankish5@gmail.com>
2021-10-22 21:20:44 +0530

    my commit

---
+++ BazelExample/BazelExample-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  BazelExample-Bridging-Header.h
+//  BazelExample
+//
+//  Created by Ankish Jain on 22/10/21.
+//
+
+#ifndef BazelExample_Bridging_Header_h
+#define BazelExample_Bridging_Header_h
+#import <PoseTrackingGpuApp/PoseTrackingViewController.h>
+
+
+#endif /* BazelExample_Bridging_Header_h */
